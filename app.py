ae89684d2863bdb4f7dee24bfb3653ece7a30dc1
Kenn Hadnot <k_hadnot@yahoo.com>
2021-01-15 23:36:22 -0800

    updated app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
-app.config["SECRET_KEY"] = "CH1LD15HG4MB1N0"
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "CH1LD15HG4MB1N0")
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
